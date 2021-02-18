94f4daa2e423e4c489093066ef9dded103adb00b
idisrajesh <rajricarp@gmail.com>
2021-02-18 14:26:57 +0530

    Data base set up for mongodb

--- InventoryServer/config/databaseConnection.js
+++ InventoryServer/config/databaseConnection.js
@@ -1,18 +1,15 @@
-const { Sequelize } = require('sequelize');
+const mongoose = require('mongoose');
 
 const connectDB = async () =>{
-    const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-        host: process.env.DB_SERVER_ADDRESS,
-        dialect: 'mssql'/* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-      });
-    
-      try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
+    const conn = await mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser:true,
+        useCreateIndex:true,
+        useFindAndModify:false,
+        useUnifiedTopology:true
+    })
+
+    console.log(`Mongodb connected: ${conn.connection.host}`)
 }
 
-module.exports =  connectDB;
+module.exports = connectDB;
 
